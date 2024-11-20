bdaae3751c8ab69d8aa526e97d4636087415cdc9
thamada <thamada@users.noreply.github.com>
2024-11-21 03:38:15 +0900

    update setDevice

--- gls/setDevice.py
+++ gls/setDevice.py
@@ -7,25 +7,30 @@ def curses_main(stdscr):
     curses.curs_set(0)
     stdscr.clear()
 
+    # 色の初期化
+    curses.start_color()
+    #curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+
     options = [
-        "NVIDIA GH200",
-        "NVIDIA GTX1050Ti",
-        "NVIDIA H100 SXM x8",
-        "NVIDIA H100 NVL x2",
-        "NVIDIA RTX 2000 Ada",
-        "NVIDIA RTX 4000 Ada",
-        "NVIDIA RTX 6000 Ada x8",
-        "NVIDIA RTX A6000 x9",
-        "NVIDIA RTX 4060 Ti",
-        "NVIDIA RTX 4090",
-        "NVIDIA L40S",
-        "NVIDIA L4",
-        "NVIDIA A100 SXM x4",
-        "NVIDIA RTX 3090",
-        "NVIDIA A40 x10",
-        "NVIDIA A10",
-        "NVIDIA V100 x8",
-        "AMD MI300X"
+        " 0: NVIDIA GH200",
+        " 1: NVIDIA GTX1050Ti",
+        " 2: NVIDIA H100 SXM x8",
+        " 3: NVIDIA H100 NVL x2",
+        " 4: NVIDIA RTX 2000 Ada",
+        " 5: NVIDIA RTX 4000 Ada",
+        " 6: NVIDIA RTX 6000 Ada x8",
+        " 7: NVIDIA RTX A6000 x9",
+        " 8: NVIDIA RTX 4060 Ti",
+        " 9: NVIDIA RTX 4090",
+        "10: NVIDIA L40S",
+        "11: NVIDIA L4",
+        "12: NVIDIA A100 SXM x4",
+        "13: NVIDIA RTX 3090",
+        "14: NVIDIA A40 x10",
+        "15: NVIDIA A10",
+        "16: NVIDIA V100 x8",
+        "17: AMD MI300X"
     ]
 
     current_row = 0
@@ -39,7 +44,7 @@ def curses_main(stdscr):
             x = width // 2 - len(options[0]) // 2  # 中央に配置するためのx座標計算（左寄せ）
             y = height // 2 - len(options) // 2 + idx  # 中央に配置するためのy座標計算
             if idx == current_row:
-                stdscr.addstr(y, x, option, curses.A_REVERSE)
+                stdscr.addstr(y, x, option, curses.color_pair(1))
             else:
                 stdscr.addstr(y, x, option)
 
@@ -51,7 +56,10 @@ def curses_main(stdscr):
             current_row += 1
         elif key == ord('\n'):
             fname_config = "~/.glsconfig"
-            create_file(fname_config, options[current_row])
+            content = ""
+            content += f"{current_row}\n"
+            content += f"{options[current_row]}\n"
+            create_file(fname_config, content)
             break
 
     stdscr.refresh()
@@ -64,17 +72,8 @@ def create_file(filename, content):
     # ファイルのディレクトリが存在するか確認し、存在しない場合は作成
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
-    '''
-    if os.path.exists(filepath):
-        overwrite = input(f"ファイル '{filepath}' は既に存在します。上書きしますか？ (y/n): ")
-        if overwrite.lower() != 'y':
-            print("\nファイル作成をキャンセルしました。")
-            return
-    '''
-
     with open(filepath, "w") as f:
         f.write(content)
-        f.write("\n")
     print(f"\nファイル '{filepath}' が作成されました。")
 
 
